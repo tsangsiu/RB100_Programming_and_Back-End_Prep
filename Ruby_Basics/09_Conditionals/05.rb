0c96a6fefed846a3e0501d6d5c67cfa34b7ddc60
Jason Tsang <tsangsiu@gmail.com>
2021-11-12 09:25:54 +0000

    Add 09_Conditionals

---
+++ Ruby_Basics/09_Conditionals/05.rb
@@ -0,0 +1,11 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# Answer:
+# The above code will output "My favourite number is 7.", as 7 is evaluated as
+# true.
